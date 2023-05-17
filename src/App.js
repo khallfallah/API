50a753b3a995f50f5215b80c59b811f32dd072f8
khallfallah <wafakhallfallah75@gmail.com>
2023-05-17 16:20:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {React , useState , useEffect} from "react";
+import axios from "axios" ;
 
-function App() {
+const App = ()  =>{
+  const [posts , setPosts]= useState([])
+  useEffect(() =>
+    {axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((reponse) => 
+
+      {
+       setPosts(reponse.data);
+      })
+      .catch((erreur) => 
+     { console.log(erreur);
+});
+
+});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}
+
+        </li>
+
+      ))}
+      </ul>
+       
+         
+     
+    
     </div>
   );
-}
+};
 
 export default App;
